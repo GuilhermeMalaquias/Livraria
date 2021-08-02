f77d057837ade769e0b27783ddd2dbba28f96054
GuilhermeMalaquias <68711421+GuilhermeMalaquias@users.noreply.github.com>
2021-08-02 15:22:13 -0300

    Update SuppresElementByAction.cs

--- src/Library.App/Extension/SuppresElementByAction.cs
+++ src/Library.App/Extension/SuppresElementByAction.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Routing;
 
 namespace Library.App.Extension
 {
+//Escondendo elemento de acordo com sua rota, evitando assim o usuario tem acesso ao botão de editar a partir da action detalhes.
     [HtmlTargetElement("*", Attributes = "supress-by-action")]
     public class SuppresElementByAction : TagHelper
     {
@@ -24,7 +25,7 @@ namespace Library.App.Extension
                 throw new ArgumentNullException(nameof(context));
             if (output == null)
                 throw new ArgumentNullException(nameof(output));
-
+           
             var action = _contextAccessor.HttpContext.GetRouteData().Values["action"].ToString();
 
             if (ActionName.Contains(action)) return;

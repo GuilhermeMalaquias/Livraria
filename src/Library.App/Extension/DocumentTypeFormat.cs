a149ada1676a284a38d2977c8e5222c678968154
GuilhermeMalaquias <68711421+GuilhermeMalaquias@users.noreply.github.com>
2021-08-02 15:23:20 -0300

    Update DocumentTypeFormat.cs

--- src/Library.App/Extension/DocumentTypeFormat.cs
+++ src/Library.App/Extension/DocumentTypeFormat.cs
@@ -7,6 +7,7 @@ namespace Library.App.Extension
     {
         public static string FormatDocument(this RazorPage page, int pessoaTipo, string documento)
         {
+            //formantando o documento de acordo com o tipo de pessoa, seja ela fisica ou juridica.
             return pessoaTipo == 1
                 ? Convert.ToUInt64(documento).ToString(@"000\.000\.000\-00")
                 : Convert.ToUInt64(documento).ToString(@"00\.000\.000\/0000\-00");
